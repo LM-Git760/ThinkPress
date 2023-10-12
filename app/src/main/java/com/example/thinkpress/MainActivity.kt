577641d853a497bb0f3ac30425bd21c9c035d161
Florian Schur <florischur@googlemail.com>
2023-10-12 11:28:09 +0200

    Bottom Navigation funktion programmiert ProfileFragment und SearchFragment erstellt

--- app/src/main/java/com/example/thinkpress/MainActivity.kt
+++ app/src/main/java/com/example/thinkpress/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.thinkpress
 import android.os.Bundle
 import android.text.TextUtils.replace
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.example.thinkpress.databinding.ActivityMainBinding
+import com.example.thinkpress.databinding.FragmentFragmentNewsBinding
+import com.example.thinkpress.databinding.FragmentSearchBinding
 import com.example.thinkpress.ui.FragmentNews
+import com.example.thinkpress.ui.ProfileFragment
+import com.example.thinkpress.ui.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -13,37 +18,53 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFragment: Fragment? = null
+
         when (item.itemId) {
             R.id.nav_home -> {
                 // Handle Home navigation
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = FragmentNews()
             }
             R.id.nav_search -> {
                 // Handle Search navigation
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = SearchFragment()
             }
             R.id.nav_profile -> {
                 // Handle Profile navigation
-                return@OnNavigationItemSelectedListener true
+                selectedFragment = ProfileFragment()
             }
         }
-        false
+        if (selectedFragment !=null){
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container, selectedFragment
+            ).commit()
+        }
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Set default fragment
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container, FragmentNews()
+        ).commit()
+
+
         binding.bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
 
         // Überprüfe, ob das Fragment bereits hinzugefügt wurde, um zu verhindern,
         // dass es bei Konfigurationsänderungen wie Drehungen erneut hinzugefügt wird
+/*
         if (savedInstanceState == null) {
             // Füge das FragmentNews zum FragmentManager hinzu
             supportFragmentManager.commit {
                 replace(R.id.fragment_container, FragmentNews.newInstance())
             }
         }
+*/
     }
 }
--- app/src/main/java/com/example/thinkpress/ui/FragmentNews.kt
+++ app/src/main/java/com/example/thinkpress/ui/FragmentNews.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.thinkpress.R
 import com.example.thinkpress.api.NewsApiService
 import com.example.thinkpress.api.NewsResult
 import com.example.thinkpress.databinding.FragmentFragmentNewsBinding
@@ -17,25 +18,11 @@ class FragmentNews : Fragment() {
     private lateinit var viewModel: NewsViewModel
     private var binding: FragmentFragmentNewsBinding? = null
 
-    companion object {
-        fun newInstance() = FragmentNews()
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentFragmentNewsBinding.inflate(inflater, container, false)
-        return binding?.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
 
         // Erstelle eine Instanz von NewsApiService
         val newsApiService = NewsApiService.create()
@@ -47,7 +34,6 @@ class FragmentNews : Fragment() {
         // Übergebe die Instanz von NewsApiService und die zusätzlichen Parameter an die NewsViewModelFactory
         val factory = NewsViewModelFactory(newsApiService, apiKey, query)
 
-
         viewModel = ViewModelProvider(this, factory).get(NewsViewModel::class.java)
 
         val adapter = NewsAdapter()
@@ -57,15 +43,21 @@ class FragmentNews : Fragment() {
             when (newsResult) {
                 is NewsResult.Success -> {
                     adapter.updateData(newsResult.articles)
-                    Log.i("NewsAdapter",newsResult.articles.toString() )
+                    Log.i("NewsAdapter", newsResult.articles.toString())
                 }
                 is NewsResult.Failure -> {
                     Log.i("NewsAdapter", newsResult.code.toString())
                 }
             }
-        }
-        )
+        })
 
         viewModel.fetchNews()
+
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
