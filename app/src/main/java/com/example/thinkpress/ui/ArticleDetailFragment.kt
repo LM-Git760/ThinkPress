53cf4b5a982f2a3d7db175b632460e9b02363d20
Florian Schur <florischur@googlemail.com>
2023-10-26 00:28:22 +0200

    Room implementation im Fragment

--- app/src/main/java/com/example/thinkpress/ui/ArticleDetailFragment.kt
+++ app/src/main/java/com/example/thinkpress/ui/ArticleDetailFragment.kt
@@ -1,29 +1,49 @@
 package com.example.thinkpress.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.thinkpress.R
+import com.bumptech.glide.Glide
 import com.example.thinkpress.api.Article
+import com.example.thinkpress.databinding.FragmentArticleDetailBinding
 
-class ArticleDetailFragment: Fragment() {
+class ArticleDetailFragment : Fragment() {
+
+    private var _binding: FragmentArticleDetailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val article = arguments?.getSerializable("article") as? Article
-        if (article == null) {
-            Log.e("ArticleDetailFragment", "Keine Artikelinformationen erhalten")
-            return null
-        }
-
-        // Hier können Sie Ihre UI-Elemente mit den Artikelinformationen füllen
-
-        return inflater.inflate(R.layout.fragment_article_detail, container, false)
+        _binding = FragmentArticleDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val article = arguments?.getSerializable("article") as? Article
+        if (article == null) {
+            // Log an error or show a toast to the user
+            return
+        }
+
+        binding.titleTextView.text = article.pubDate
+        binding.authorTextView.text = article.title
+        binding.descriptionTextView.text = article.description
+        binding.contentTextView.text = article.content
+
+        Glide.with(this)
+            .load(article.imageUrl)
+            .into(binding.articleImageView)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
